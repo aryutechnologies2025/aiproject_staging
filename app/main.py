c824ddc5e3e3d236222e5b85af14f1b47d3f698d
ak-z-world <aryutechnologies2024@gmail.com>
2026-06-30 15:34:53 +0530

    updates

--- app/main.py
+++ app/main.py
@@ -90,9 +90,12 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "http://127.0.0.1:8001",
         "https://passats.aryuacademy.com",
         "https://ai.aryuacademy.com",
+        "https://portal.aryuacademy.com",
+        "https://aylms.aryuprojects.com",
+
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE","OPTIONS"],
